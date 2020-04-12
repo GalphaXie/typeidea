d276f0bba490536cdd231ba9cff5241606f61336
guang <XIEGalpha@163.com>
2020-04-12 15:27:16 +0800

    feat(serializer & views): complete list and detail api

--- typeidea/blog/serializers.py
+++ typeidea/blog/serializers.py
@@ -4,6 +4,17 @@ from blog.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    tag = serializers.SlugRelatedField(many=True, slug_field='name', read_only=True)
+    owner = serializers.SlugRelatedField(read_only=True, slug_field='username')
+    created_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+
     class Meta:
         model = Post
-        fields = ['title', 'category', 'desc', 'content_html', 'created_time']
+        fields = ['id', 'title', 'category', 'tag', 'owner', 'created_time']
+
+
+class PostDetailSerializer(PostSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'category', 'tag', 'owner', 'content_html', 'created_time']
