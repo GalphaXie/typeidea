5b12f11cd71b1859fae51a742fb0f65744f7d859
guang <XIEGalpha@163.com>
2020-04-14 21:56:33 +0800

    feat(all): debuger or optimizal

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -58,3 +58,14 @@ urlpatterns = [
     url(r'^api/docs/', include_docs_urls(title='typeidea apis', description="CORE API DOCUMENTS")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    # import debug_toolbar
+    urlpatterns += [
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
+    urlpatterns += [
+        url(r'^silk/', include('silk.urls', namespace="silk"))
+    ]
+
+
